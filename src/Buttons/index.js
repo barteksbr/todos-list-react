a38dd228031391d20f815a17536300b44e25a5da
Bartosz Owczarzak <barteksbr@gmail.com>
2023-01-20 10:37:33 +0100

    formated all document

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,24 +1,20 @@
 import "./style.css";
 
-const Buttons = ({tasks, hideDoneTask}) => {
-    if(tasks.lenght === 0) {
+const Buttons = ({ tasks, hideDoneTask }) => {
+    if (tasks.lenght === 0) {
         return null
     }
-    
     return (
-
         <div className="buttons">
             <button className="button__button ">
-    {hideDoneTask ? "Pokaż" : "Ukryj"} Ukończone </button>
-    <button
-    className="button__button"
-    disabled={tasks.every(({ done }) => done)}
-        
-    >
-    Ukończ Wszystkie
-    </button>
+                {hideDoneTask ? "Pokaż" : "Ukryj"} Ukończone </button>
+            <button
+                className="button__button"
+                disabled={tasks.every(({ done }) => done)}
+            >
+                Ukończ Wszystkie
+            </button>
         </div>
-
     )
 }
 
--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -6,15 +6,13 @@ const Tasks = (props) => (
         {props.tasks.map(task => (
             <li
                 className={`task__item${task.done && props.hideDoneTask ? " tasks__item--hiden" : ""}`}
-
             >
                 <button className="tasks__button tasks__button--done">
-            {task.done ? "✓" : ""}
-            </button>
-            <span className={`"tasks__content${task.done ? " tasks__content--done" : ""}"`}>
-            {task.content}</span>
-
-            <button className="tasks__button tasks__button--remove">🗑️</button>
+                    {task.done ? "✓" : ""}
+                </button>
+                <span className={`tasks__content${task.done ? " tasks__content--done" : ""}`}>
+                    {task.content}</span>
+                <button className="tasks__button tasks__button--remove">🗑️</button>
             </li>
         ))}
     </ul>
