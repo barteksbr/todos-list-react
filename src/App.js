4d42d399ddd12b3676d3d3659ac0a0314107bed8
Bartosz Owczarzak <barteksbr@gmail.com>
2023-01-25 15:08:13 +0100

    addNewTask interaction

--- src/App.js
+++ src/App.js
@@ -42,12 +42,23 @@ function App() {
     })));
   }
 
+  const addNewTask = (content) => {
+    setTasks(tasks => [
+      ...tasks,
+      {
+        content,
+        done: false,
+        id: tasks.length === 0 ? 1 : tasks[tasks.length - 1].id + 1,
+      }
+    ])
+  }
+
   return (
     <Container>
       <h1>Lista zadań</h1>
       <Section
         title="Dodaj nowe zadanie"
-        body={<Form />}
+        body={<Form addNewTask={addNewTask} />}
       />
 
       <Section
