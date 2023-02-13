824b3e56c83103c7a7028ff2f9a09fd94a5732d0
Bartosz Owczarzak <barteksbr@gmail.com>
2023-02-13 12:34:45 +0100

    added FOCUS on INPUT

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,14 +1,16 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { StyledForm, Input, Button } from "./styled";
 
 
 const Form = ({ addNewTask }) => {
     const [newTaskContent, setNewTaskContent] = useState("");
+    const inputRef = useRef(null);
 
     const onFormSubmit = (event) => {
         event.preventDefault();
         addNewTask(newTaskContent.trim());
         setNewTaskContent("");
+        inputRef.current.focus();
     };
 
     return (
@@ -16,6 +18,7 @@ const Form = ({ addNewTask }) => {
             onSubmit={onFormSubmit}
         >
             <Input
+                ref={inputRef}
                 value={newTaskContent}
                 placeholder="Co jest do zrobienia?"
                 onChange={(event) => setNewTaskContent(event.target.value)}
